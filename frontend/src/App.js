44c5305178d07cc200b7730d8889255e32b2c367
Sreeram A <sriramlibra0@gmail.com>
2023-03-30 22:19:30 +0530

    leader board / coupons page ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
+import Coupons from "./components/Coupons";
 import CouponsPages from "./pages/DiscoundsPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    // <div className="flex flex-col   justify-center items-center h-[200vh]  w-[100vw] box-border">
-    <div className="flex    justify-center items-center h-[100vh] overflow-hidden  w-[100vw] box-border">
-{/* 
+    <div className="flex flex-col   justify-center items-center h-[400vh]  w-[100vw] box-border">
+    {/* // <div className="flex    justify-center items-center h-[100vh] overflow-hidden  w-[100vw] box-border"> */}
+    
     <LoginPage/>
-    <HomePage/> */}
+    <HomePage/>
     <CouponsPages/>
+    {/* <Coupons/> */}
     </div>
   )
 }
--- frontend/src/components/Score.js
+++ frontend/src/components/Score.js
@@ -2,27 +2,31 @@ import React from "react";
 
 const Score = () => {
   return (
-    <div className="flex flex-col items-center  justify-start h-[100%] w-[100%] gap-5 bg-red-100 ">
+    <div className="flex flex-col items-center  justify-start h-[100vh] w-[100vw] gap-5 bg-white ">
       <p className="text-4xl mt-5 ">
         <span>
           <i class="fa-solid fa-angle-left mr-3"></i>
         </span>
         Leaderboard
       </p>
-      <div className="flex flex-col items-center justify-between w-[100%] h-[100%] ">
+      <div className="flex flex-col items-center justify-between w-[100%]  mt-5 max-w-lg h-[100%] ">
         {/* ranklist */}
-        <div className="flex h-[65%] bg-white">
-          <ul>
-            <li>Name rank </li>
-            <li>Name rank </li>
-            <li>Name rank </li>
+        <div className="flex h-[65%] w-[90%] bg-white">
+          <ul className="w-[100%] flex flex-col gap-2">
+            <li className=" bg-indigo-300 w-[100%] px-5 py-2 rounded-lg"> <span  bg-blue-100 > #{1 }</span> Sreeram</li>
+            <li className=" bg-indigo-300 w-[100%] px-5 py-2 rounded-lg"> <span> #{1 }</span> Sreeram</li>
+            <li className=" bg-indigo-300 w-[100%] px-5 py-2 rounded-lg"> <span> #{1 }</span> Sreeram</li>
+            <li className=" bg-indigo-300 w-[100%] px-5 py-2 rounded-lg"> <span> #{1 }</span> Sreeram</li>
+            <li className=" bg-indigo-500 w-[100%] px-5 py-2 rounded-lg"> <span> #{1 }</span> Sreeram</li>
+
+         
           </ul>
         </div>
 
         {/* referral link */}
 
         <div className="flex flex-col w-[90%]  h-[30%]   max-w-lg ">
-          <div className="flex  items-center justify-center border-3 rounded-lg shadow-lg mb-4 bg-pink-600 p-5">
+          <div className="flex  flex-col  items-center justify-center border-3 rounded-lg shadow-lg mb-4 bg-indigo-600 p-5">
             <p className="text-lg font-bold text-orange-200">
               <span>
                 <i class="fa-regular fa-clipboard mr-3"></i>
@@ -31,7 +35,7 @@ const Score = () => {
             </p>
           </div>
           {/* infstruction */}
-          <div className="w-[90%] mx-auto">
+          <div className="flex flex-col items-center justify-center">
             <ul>
               <li>25% off on the iphone 14 for #1</li>
               <li>Earn 10 poits on each referral </li>
